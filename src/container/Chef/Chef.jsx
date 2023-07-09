e81dd3265dd5cdc77c89e8b49eea7ec94096d2ab
Austin okonkwo <austinokonkwo1995@gmail.com>
2023-07-09 14:41:35 +0100

    second commit

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import {images} from '../../constants';
+import './Chef.css';
+
+const Chef = () => (
+  <div className='app__bg app__wrapper section__padding'>
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What We Belive In</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">Lorem ipsum dolor sit amet, sit amet consectetur adipisicing consectetur adipisicing elit.</p>
+          </div>
+          <p className="p__opensans">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint ex eveniet, exercitationem quis impedit illum nam aut iusto ut. Maxime.</p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Austin Okonkwo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
